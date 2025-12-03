3ae6136e63a2220846ca3b5934541294bbbe44ce
Andrei Vialichka <andreivalichka@onlineimmigrant.rs>
2025-12-03 21:53:35 +0100

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -304,7 +304,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         {/* DNS Prefetch & Preconnect for performance */}
         <link rel="dns-prefetch" href="https://rgbmdfaoowqbgshjuwwm.supabase.co" />
         <link rel="preconnect" href="https://rgbmdfaoowqbgshjuwwm.supabase.co" crossOrigin="anonymous" />
+        {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        {/* eslint-disable-next-line @next/next/google-font-preconnect */}
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -315,14 +317,16 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         
         {/* Async load Google Fonts - won't block LCP */}
         {/* Using onLoad inline handler instead of script to avoid hydration issues */}
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&family=Poppins:wght@300;400;500;600;700&family=Lato:wght@300;400;700&family=Open+Sans:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700&family=Nunito:wght@300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&family=Ubuntu:wght@300;400;500;700&family=Merriweather:wght@300;400;700&display=swap"
           media="print"
-          // @ts-ignore - onLoad is valid for link elements
+          // @ts-expect-error - onLoad is valid for link elements but not in TypeScript HTMLLinkElement interface
           onLoad="this.media='all'"
         />
         <noscript>
+          {/* eslint-disable-next-line @next/next/no-page-custom-font */}
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&family=Poppins:wght@300;400;500;600;700&family=Lato:wght@300;400;700&family=Open+Sans:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700&family=Nunito:wght@300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&family=Ubuntu:wght@300;400;500;700&family=Merriweather:wght@300;400;700&display=swap"
